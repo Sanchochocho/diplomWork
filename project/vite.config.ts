8fec7aaa988a1bee6ca93ff74b661e2748310fef
Ilya Vedyanin <yzbekalesha@dmail.com>
2025-11-16 23:36:35 +0500

    transmitting the recipes table

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Любой запрос к /users на фронтенде будет проксирован на порт 5000
+      '/users': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/users/, '/users')
+      }
+    }
+  }
 })
+
--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 app.get("/users", async (req, res) => {
   const { data, error } = await supabase
-  .from("users")
+  .from("recipes")
   .select("*");
 
   if (error) {
